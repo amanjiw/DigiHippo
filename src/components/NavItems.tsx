ac289b14e9a7d0101b0744170f5bec7f398a166e
amanj ghaderi <ghaderi.amanj@gmail.com>
2024-06-05 21:56:10 -0700

    navbar items added

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,10 +1,39 @@
 "use client";
 
-import { useState } from "react";
+import { PRODUCT_CATEGORYS } from "@/config";
+import { useMemo, useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
 	const [activeIndex, setActiveIndex] = useState<null | number>(null);
-	return <div className="flex gap-4 h-full"></div>;
+
+	const isAnyOpen = useMemo(() => activeIndex !== null, [activeIndex]);
+
+	return (
+		<div className="flex gap-4 h-full">
+			{PRODUCT_CATEGORYS.map((category, index) => {
+				const handleOpen = () => {
+					if (activeIndex === index) {
+						setActiveIndex(null);
+					} else {
+						setActiveIndex(index);
+					}
+				};
+
+				const isOpen = index === activeIndex;
+
+				return (
+					<NavItem
+						key={""}
+						category={category}
+						handleOpen={handleOpen}
+						isOpen={isOpen}
+						isAnyOpen={isAnyOpen}
+					/>
+				);
+			})}
+		</div>
+	);
 };
 
 export default NavItems;
