d72e00ea1f1ac7b2a3047fdb59a4b9de975336fb
amanj ghaderi <ghaderi.amanj@gmail.com>
2024-06-06 20:35:06 -0700

    close navbar with escape button and clicking outside navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,29 @@
 "use client";
 
 import { PRODUCT_CATEGORYS } from "@/config";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/useOnClickOutside";
 
 const NavItems = () => {
 	const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+	useEffect(() => {
+		const handler = (event: KeyboardEvent) => {
+			if (event.key === "Escape") setActiveIndex(null);
+		};
+
+		document.addEventListener("keydown", handler);
+
+		return () => document.removeEventListener("keydown", handler);
+	}, []);
+
 	const isAnyOpen = useMemo(() => activeIndex !== null, [activeIndex]);
+	const navRef = useRef<HTMLDivElement | null>(null);
+	useOnClickOutside(navRef, () => setActiveIndex(null));
 
 	return (
-		<div className="flex gap-4 h-full">
+		<div className="flex gap-4 h-full" ref={navRef}>
 			{PRODUCT_CATEGORYS.map((category, index) => {
 				const handleOpen = () => {
 					if (activeIndex === index) {
